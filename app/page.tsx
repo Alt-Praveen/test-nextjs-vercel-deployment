c04d25e9785d38b350b2dd790d21b9ada146a66c
Alt_Praveen <43923651+Alt-Praveen@users.noreply.github.com>
2025-12-02 12:14:14 +0530

    re upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
+// import MainPage from ".";
 
 export default function Home() {
   const getEnvVar = process.env.TEST_VARIABLE
   console.log("TEST_VARIABLE in env file => ", getEnvVar);
 
   return (
+    // <MainPage envVar={getEnvVar} />
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-            Hello World
+            Hello World `${getEnvVar || ' undefined '}` This is test.
           </code>
         </div>
       </main>
