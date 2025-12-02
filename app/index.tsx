ca1b255ab5f474d0e022cb553b587f81a129fa11
Alt_Praveen <43923651+Alt-Praveen@users.noreply.github.com>
2025-12-02 12:38:42 +0530

    test new env variables

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,21 @@
 'use client'
 
 type EnvVar = {
-    envVar: string | undefined
+    envVar?: string | undefined,
+    clientVar?: string | undefined
 }
 
-export default function MainPage({ envVar }: EnvVar) {
+export default function MainPage({ envVar, clientVar }: EnvVar) {
 
     return (
         <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
             <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
                 <div className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
                     <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-                        Hello World `${envVar || ' undefined '}` This is test.
+                        Hello World
                     </code>
+                    <p>Test Variable = {envVar}</p>
+                    <p>Client Variable = {clientVar}</p>
                 </div>
             </main>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,12 @@
-// import MainPage from ".";
+import MainPage from ".";
 
 export default function Home() {
   const getEnvVar = process.env.TEST_VARIABLE
   console.log("TEST_VARIABLE in env file => ", getEnvVar);
+  const getClientVar = process.env.CLIENT_VAR
+  console.log("CLIENT_VAR in env file => ", getClientVar);
 
   return (
-    // <MainPage envVar={getEnvVar} />
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-            Hello World `${getEnvVar || ' undefined '}` This is test.
-          </code>
-        </div>
-      </main>
-    </div>
+    <MainPage envVar={getEnvVar} clientVar={getClientVar} />
   );
 }
